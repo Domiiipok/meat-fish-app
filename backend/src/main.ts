07c7c4ed234f7a9069b7477955289bb696b6c99b
Domiiipok <Domipok963747@gmail.com>
2025-06-18 23:09:27 +0300

    add Supabase user service

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+
+  // ⛔️ Важно: разрешаем CORS с фронта
+  app.enableCors({
+    origin: 'https://meat-fish-frontend.onrender.com', // или '*', но лучше точно
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
